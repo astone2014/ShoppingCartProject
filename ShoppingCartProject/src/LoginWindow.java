c2c2888fbf067c89c76c5e2b275bbbd9604999f8
Austen Stone <astone2014@fau.edu>
2017-04-18 22:25:06 -0400

    coming from laptop

--- ShoppingCartProject/src/LoginWindow.java
+++ ShoppingCartProject/src/LoginWindow.java
@@ -1,8 +1,41 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
-public class LoginWindow {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
 
+public class LoginWindow extends JFrame {
+
+	/**
+	 * Launch the application.
+	 */
 	public static void main(String[] args) {
-		System.out.print("hello");
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginWindow frame = new LoginWindow();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public LoginWindow() {
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		
+		Login login = new Login();
+		login.setVisible(true);
+		getContentPane().add(login);
+		
 	}
 
 }
